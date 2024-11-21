eef9f1ad4b044a5387063e99f1ba39410fd6bfd4
riedhampattel <riedhampattel.tops@gmail.com>
2024-11-21 09:23:42 +0530

    updated

---
+++ C Programming/Basic syntax/1.c
@@ -0,0 +1,18 @@
+// single line comment
+/*
+
+	multiline comment
+	# ---> pre - processor
+	include ---> directive
+	stdio.h ---> standard input output header file
+	int             main()
+	return type     function name
+
+*/
+#include<stdio.h>
+int main()
+{
+	printf("Hello \tworld!");
+	printf("\nHow are you?");
+	return 0;
+}
