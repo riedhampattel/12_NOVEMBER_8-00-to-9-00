bf8ba931888595771bc08d4c91f7312eaac76c4c
riedhampattel <riedhampattel.tops@gmail.com>
2024-12-17 10:43:00 +0530

    updated

--- C Programming/Array/12.c
+++ C Programming/Array/12.c
@@ -1,94 +1,67 @@
-//Write a c program to take row and column numbers of 2D array and elements from the user and make one calculator with addition, subtraction and multiplication facilities using control statement.
+//Write a c program to take row and column numbers of 2D array and elements from the user and perform the multiplication
 #include<stdio.h>
 int main()
 {
-	int row,col,a[100][100],b[100][100],ans[100][100];
-	int i,j,choice;
+	int a[100][100],b[100][100],ans[100][100];
+	int size,i,j,k;
 	
-	printf("\nEnter the row number = ");
-	scanf("%d",&row);
-	printf("\nEnter the col number = ");
-	scanf("%d",&col);
+	printf("\nEnter the ize of an array = ");
+	scanf("%d",&size);
 	
-	for(i=0;i<row;i++)
+	for(i=0;i<size;i++)
 	{
-		for(j=0;j<col;j++)
+		for(j=0;j<size;j++)
 		{
 			printf("\nEnter the element in a[%d][%d] = ",i,j);
 			scanf("%d",&a[i][j]);
 		}
 	}
 	printf("\n");
-	for(i=0;i<row;i++)
+	for(i=0;i<size;i++)
 	{
-		for(j=0;j<col;j++)
+		for(j=0;j<size;j++)
 		{
 			printf("\nEnter the element in b[%d][%d] = ",i,j);
 			scanf("%d",&b[i][j]);
 		}
 	}
 	printf("\nArray a :=\n");
-	for(i=0;i<row;i++)
+	for(i=0;i<size;i++)
 	{
-		for(j=0;j<col;j++)
+		for(j=0;j<size;j++)
 		{
 			printf("%d ",a[i][j]);
 		}
 		printf("\n");
 	}
 	printf("\nArray b :=\n");
-	for(i=0;i<row;i++)
+	for(i=0;i<size;i++)
 	{
-		for(j=0;j<col;j++)
+		for(j=0;j<size;j++)
 		{
 			printf("%d ",b[i][j]);
 		}
 		printf("\n");
 	}
-	printf("\n\nPress '+' for Addition");
-	printf("\nPress '-' for Subtraction");
-	printf("\nEnter your choice = ");
-	scanf(" %c",&choice);
-	
-	switch(choice)
+	for(i=0;i<size;i++)
 	{
-		case '+':
-			for(i=0;i<row;i++)
-			{
-				for(j=0;j<col;j++)
-				{
-					ans[i][j] = a[i][j] + b[i][j];
-				}
-			}
-			printf("\nThe addition is := \n");
-			for(i=0;i<row;i++)
-			{
-				for(j=0;j<col;j++)
-				{
-					printf("%d ",ans[i][j]);
-				}
-				printf("\n");
-			}
-		break;
-		case '-':
-			for(i=0;i<row;i++)
-			{
-				for(j=0;j<col;j++)
-				{
-					ans[i][j] = a[i][j] - b[i][j];
-				}
-			}
-			printf("\nThe addition is := \n");
-			for(i=0;i<row;i++)
-			{
-				for(j=0;j<col;j++)
-				{
-					printf("%d ",ans[i][j]);
-				}
-				printf("\n");
-			}
-		break;
-		default : printf("\nInvalid input!");
+		for(j=0;j<size;j++)
+		{
+            ans[i][j] = 0;
+            for(k=0;k<size;k++)
+            {
+                ans[i][j] = ans[i][j] + (a[i][k]*b[k][j]);
+            }
+		}
+	}
+    printf("\nArray ans :=\n");
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<size;j++)
+		{
+			printf("%d ",ans[i][j]);
+		}
+		printf("\n");
 	}
 	return 0;
 }
